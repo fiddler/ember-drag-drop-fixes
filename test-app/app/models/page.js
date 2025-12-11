34f9829fa09331ce7f324fc58cbeb52fa04bee01
Jussi Kaijalainen <jussi@wingmen.fi>
2025-12-11 11:55:19 +0800

    Flatten monorepo to single package at root
    Enables direct GitHub install without ?path= workaround.
    npm doesn't properly support ?path= for GitHub URLs.

--- test-app/app/models/page.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable ember/no-classic-classes, ember/use-ember-data-rfc-395-imports */
-import DS from 'ember-data';
-
-var Page = DS.Model.extend({
-  title: DS.attr('string'),
-});
-
-export default Page;
